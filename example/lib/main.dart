e5199e37104d84f9dcbff0aabbb1b1d63f84080f
Adrien Bonnin <adbonnin@users.noreply.github.com>
2020-12-31 10:44:57 +0100

    :sparkles: Add the horizontal divider

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,89 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_horizontal_divider/flutter_horizontal_divider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(HorizontalDividerExample());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterHorizontalDivider.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class HorizontalDividerExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final color = Colors.blue;
+    final size = 10.0;
+    final thickness = 4.0;
+    final padding = 20.0;
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        appBar: AppBar(title: Text('Horizontal divider example')),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ...divided(
+                context,
+                title: 'Horizontal divider',
+                scrollDirection: Axis.vertical,
+                divider: HorizontalDivider(
+                  color: color,
+                  height: size,
+                  thickness: thickness,
+                  indent: padding,
+                  endIndent: padding,
+                ),
+              ),
+              SizedBox(height: 20),
+              ...divided(
+                context,
+                title: 'Vertical divider',
+                scrollDirection: Axis.horizontal,
+                divider: VerticalDivider(
+                  color: color,
+                  width: size,
+                  thickness: thickness,
+                  indent: padding,
+                  endIndent: padding,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  List<Widget> divided(
+    BuildContext context, {
+    String title,
+    Axis scrollDirection,
+    Widget divider,
+  }) {
+    return [
+      Padding(
+        padding: EdgeInsets.only(bottom: 5.0),
+        child: Text(
+          title,
+          textAlign: TextAlign.start,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black38),
+          ),
+          child: ListView(
+            scrollDirection: scrollDirection,
+            children: [
+              Icon(Icons.music_note, color: Colors.green),
+              divider,
+              Icon(Icons.pets, color: Colors.pink),
+            ],
+          ),
+        ),
+      )
+    ];
   }
 }
--- lib/flutter_horizontal_divider.dart
+++ lib/flutter_horizontal_divider.dart
@@ -1,18 +1,105 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-import 'dart:async';
+/// Based on flutter [VerticalDivider]
+/// A thin horizontal line, with padding on either side.
+///
+/// In the material design language, this represents a divider. Horizontal
+/// dividers can be used in vertically scrolling lists, such as a
+/// [ListView] with [ListView.scrollDirection] set to [Axis.vertical].
+///
+/// The box's total height is controlled by [height]. The appropriate
+/// padding is automatically computed from the height.
+///
+/// See also:
+///
+///  * [VerticalDivider], the original vertical divider.
+///  * [ListView.separated], which can be used to generate vertical dividers.
+///  * <https://material.io/design/components/dividers.html>
+class HorizontalDivider extends StatelessWidget {
+  /// Creates a material design horizontal divider.
+  ///
+  /// The [height], [thickness], [indent], and [endIndent] must be null or
+  /// non-negative.
+  const HorizontalDivider({
+    Key? key,
+    this.height,
+    this.thickness,
+    this.indent,
+    this.endIndent,
+    this.color,
+  }) : assert(height == null || height >= 0.0),
+       assert(thickness == null || thickness >= 0.0),
+       assert(indent == null || indent >= 0.0),
+       assert(endIndent == null || endIndent >= 0.0),
+       super(key: key);
 
-import 'package:flutter/services.dart';
+  /// The divider's height.
+  ///
+  /// The divider itself is always drawn as a horizontal line that is centered
+  /// within the height specified by this value.
+  ///
+  /// If this is null, then the [DividerThemeData.space] is used. If that is
+  /// also null, then this defaults to 16.0.
+  final double? height;
 
-class FlutterHorizontalDivider {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_horizontal_divider');
+  /// The thickness of the line drawn within the divider.
+  ///
+  /// A divider with a [thickness] of 0.0 is always drawn as a line with a
+  /// width of exactly one device pixel.
+  ///
+  /// If this is null, then the [DividerThemeData.thickness] is used which
+  /// defaults to 0.0.
+  final double? thickness;
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// The amount of empty space at the left of the divider.
+  ///
+  /// If this is null, then the [DividerThemeData.indent] is used. If that is
+  /// also null, then this defaults to 0.0.
+  final double? indent;
+
+  /// The amount of empty space at the right of the divider.
+  ///
+  /// If this is null, then the [DividerThemeData.endIndent] is used. If that is
+  /// also null, then this defaults to 0.0.
+  final double? endIndent;
+
+  /// The color to use when painting the line.
+  ///
+  /// If this is null, then the [DividerThemeData.color] is used. If that is
+  /// also null, then [ThemeData.dividerColor] is used.
+  ///
+  /// {@tool snippet}
+  ///
+  /// ```dart
+  /// Divider(
+  ///   color: Colors.deepOrange,
+  /// )
+  /// ```
+  /// {@end-tool}
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    final DividerThemeData dividerTheme = DividerTheme.of(context);
+    final double height = this.height ?? dividerTheme.space ?? 16.0;
+    final double thickness = this.thickness ?? dividerTheme.thickness ?? 0.0;
+    final double indent = this.indent ?? dividerTheme.indent ?? 0.0;
+    final double endIndent = this.endIndent ?? dividerTheme.endIndent ?? 0.0;
+
+    return SizedBox(
+      height: height,
+      child: Center(
+        child: Container(
+          height: thickness,
+          margin: EdgeInsetsDirectional.only(start: indent, end: endIndent),
+          decoration: BoxDecoration(
+            border: Border(
+              top: Divider.createBorderSide(context, color: color, width: thickness),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
